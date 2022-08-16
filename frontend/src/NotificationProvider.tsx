db2dcf5604a609505d6f54328288727085fb1983
Gurleen Sethi <sarusethi@rocketmail.com>
2022-08-15 20:10:51 -0400

    feat: fix notification sounds

--- frontend/src/NotificationProvider.tsx
+++ frontend/src/NotificationProvider.tsx
@@ -1,16 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { EventsOff, EventsOn } from "../wailsjs/runtime/runtime";
+import sound from "./assets/audio/timer-notification.mp3";
 
 export const NotificationProvider = (props: { children: React.ReactNode }) => {
+  const [error, setError] = useState("");
+
   useEffect(() => {
     EventsOn("notification_sound", (type) => {
       if (type === "timer_finished") {
-        new Audio("/src/assets/audio/timer-notification.mp3").play();
+        try {
+          new Audio(sound).play();
+        } catch (err) {
+          setError(err + "");
+        }
       }
     });
 
     return () => EventsOff("notification_sound");
   }, []);
 
-  return <>{props.children}</>;
+  return (
+    <>
+      {error}
+      {props.children}
+    </>
+  );
 };
